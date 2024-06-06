1cf59677a2acd151853d8473f4c2fd54d641ac6c
Naidu A R <swetha.naidu.a.r@accenture.com>
2024-06-06 21:59:41 +0530

    overall commit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,12 @@ import salesRoutes from './routes/sales.js'
 
 // data imports
 import User from "./models/User.js"
-import { dataUser } from "./data/index.js"
+import Product from "./models/Product.js"
+import ProductStat from './models/ProductStat.js'; 
+import Transaction from './models/Transaction.js'
+import OverallStat from './models/OverallStat.js';
+import AffiliateStat from './models/AffiliateStat.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from "./data/index.js"
 
 /* CONFIGURATION */
 dotenv.config();
@@ -42,6 +47,12 @@ mongoose.connect(process.env.MONGO_URL, {
     
     /* ONLY ADD DATA ONE TIME */
 //User.insertMany(dataUser);
+//Product.insertMany(dataProduct);
+//ProductStat.insertMany(dataProductStat);
+// Transaction.insertMany(dataTransaction);
+// OverallStat.insertMany(dataOverallStat);
+// AffiliateStat.insertMany(dataAffiliateStat);
+
 
 
 }).catch((error) => console.log(`${error} Did not connect`))
